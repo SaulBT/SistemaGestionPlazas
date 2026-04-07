266ae8a0c06dba1bd40005e3e6d054cbc6e73176
SaulBT <barragansaul26n@gmail.com>
2026-04-07 12:26:56 -0600

    feat: se agregaron constantes con los nombres de los roles

---
+++ SGPla/Commons/Constantes.cs
@@ -0,0 +1,8 @@
+﻿namespace SGPla.Commons
+{
+    public class Constantes
+    {
+        public const string CoordinadorEa = "Coordinador de Entidad Académica";
+        public const string CoordinadorDgaa = "Coordinador de Área Académica";
+    }
+}
