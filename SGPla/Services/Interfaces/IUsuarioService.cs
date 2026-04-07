9d39bab046fc86f2e451c116e7f85f691e49d69c
SaulBT <barragansaul26n@gmail.com>
2026-04-07 11:54:53 -0600

    feat: Interfaz del servicio de Usuarios

---
+++ SGPla/Services/Interfaces/IUsuarioService.cs
@@ -0,0 +1,19 @@
+﻿using SGPla.Models.DTOs.Usuario;
+
+namespace SGPla.Services.Interfaces
+{
+    public interface IUsuarioService
+    {
+        Task<int> CrearAsync(CrearUsuarioDTO dto);
+
+        Task<List<ListaUsuarioDTO>> ObtenerTodosAsync();
+
+        Task<List<ListaUsuarioDTO>> ObtenerPorFiltroAsync(FiltrosUsuarioDTO filtro);
+
+        Task<DetallesUsuarioDTO?> ObtenerPorIdAsync(int idUsuario);
+
+        Task EditarAsync(EditarUsuarioDTO dto);
+
+        Task EliminarAsync(int idUsuario);
+    }
+}
