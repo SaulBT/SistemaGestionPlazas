fa896f21f6d0c3a92aa1005fe716f9c1bd1e51c3
LPablo-NRG <luispablolagunesnoriega@gmail.com>
2026-04-08 13:52:49 -0600

    feat: se creó la plantilla del layout de SuperUsuario y se modificó el site

---
+++ SGPla/Controllers/UsuariosController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SGPla.Models;
+using SGPla.Services.Interfaces;
+using SGPla.Services.Implementations;
+
+namespace SGPla.Controllers
+{
+
+    public class UsuariosController : Controller
+    {
+        private readonly IUsuarioService _usuarioService;
+        
+        public UsuariosController(IUsuarioService usuarioService)
+        {
+            _usuarioService = usuarioService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        /*
+        public async Task<IActionResult> Index()
+        {
+            var usuarios = await _usuarioService.ObtenerTodosAsync();
+            return View(usuarios);
+        }*/
+        /*
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                await _usuarioService.CrearUsuarioAsync(usuario);
+            }
+            return RedirectToAction("Index");
+        }*/
+    }
+}
