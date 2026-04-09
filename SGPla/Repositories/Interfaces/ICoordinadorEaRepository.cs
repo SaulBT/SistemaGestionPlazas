26051238db06a54c2d716974a3580f1b943afa82
SaulBT <barragansaul26n@gmail.com>
2026-04-09 11:57:37 -0600

    up: validar correo repetido en CoordinadorEaRepository

--- SGPla/Repositories/Interfaces/ICoordinadorEaRepository.cs
+++ SGPla/Repositories/Interfaces/ICoordinadorEaRepository.cs
@@ -5,6 +5,7 @@ namespace SGPla.Repositories.Interfaces
     public interface ICoordinadorEaRepository
     {
         Task<int> CrearAsync(CoordinadorEa coordinadorEa);
+        Task<bool> ExisteCorreoAsync(string correo);
         Task<CoordinadorEa?> ObtenerPorIdAsync(int idCoordinadorEa);
         Task<List<CoordinadorEa>> ObtenerTodosAsync();
         Task<List<CoordinadorEa>> ObtenerPorFiltrosAsync(string? region, int? idAreaAcademica, int? idEntidadAcademica);
