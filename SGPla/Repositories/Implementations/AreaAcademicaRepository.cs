104b667c684c791ecc42d3e1b4226d008fd739af
SaulBT <barragansaul26n@gmail.com>
2026-04-09 11:43:19 -0600

    feat: repositorio de AreaAcademica

---
+++ SGPla/Repositories/Implementations/AreaAcademicaRepository.cs
@@ -0,0 +1,54 @@
+﻿namespace SGPla.Repositories.Implementations
+{
+    using Microsoft.EntityFrameworkCore;
+    using SGPla.Data;
+    using SGPla.Models;
+    using SGPla.Repositories.Interfaces;
+
+    public class AreaAcademicaRepository : IAreaAcademicaRepository
+    {
+        private readonly GestionDePlazasDbContext _context;
+
+        public AreaAcademicaRepository(GestionDePlazasDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<AreaAcademica>> ObtenerTodosAsync()
+        {
+            return await _context.AreaAcademica
+                .AsNoTracking()
+                .OrderBy(a => a.Nombre)
+                .ToListAsync();
+        }
+
+        public async Task<AreaAcademica?> ObtenerPorIdAsync(int idAreaAcademica)
+        {
+            return await _context.AreaAcademica
+                .AsNoTracking()
+                .FirstOrDefaultAsync(a => a.IdAreaAcademica == idAreaAcademica);
+        }
+
+        public async Task<bool> ExistePorIdAsync(int idAreaAcademica)
+        {
+            if (idAreaAcademica <= 0)
+                return false;
+
+            return await _context.AreaAcademica
+                .AnyAsync(a => a.IdAreaAcademica == idAreaAcademica);
+        }
+
+        public async Task<AreaAcademica> CrearAsync(AreaAcademica areaAcademica)
+        {
+            await _context.AreaAcademica.AddAsync(areaAcademica);
+            await _context.SaveChangesAsync();
+            return areaAcademica;
+        }
+
+        public async Task ActualizarAsync(AreaAcademica areaAcademica)
+        {
+            _context.AreaAcademica.Update(areaAcademica);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ SGPla/Repositories/Interfaces/IAreaAcademicaRepository.cs
@@ -0,0 +1,13 @@
+﻿using SGPla.Models;
+
+namespace SGPla.Repositories.Interfaces
+{
+    public interface IAreaAcademicaRepository
+    {
+        Task<List<AreaAcademica>> ObtenerTodosAsync();
+        Task<AreaAcademica?> ObtenerPorIdAsync(int idAreaAcademica);
+        Task<bool> ExistePorIdAsync(int idAreaAcademica);
+        Task<AreaAcademica> CrearAsync(AreaAcademica areaAcademica);
+        Task ActualizarAsync(AreaAcademica areaAcademica);
+    }
+}
