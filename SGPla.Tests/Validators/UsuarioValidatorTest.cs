668c03b3fee08adc0e61e2a853c4f3deb5fb2b16
SaulBT <barragansaul26n@gmail.com>
2026-04-10 13:06:47 -0600

    up: Se agregaron todas las pruebas de la validación

--- SGPla.Tests/Validators/UsuarioValidatorTest.cs
+++ SGPla.Tests/Validators/UsuarioValidatorTest.cs
@@ -3,6 +3,7 @@ using SGPla.Repositories.Interfaces;
 using SGPla.Models.DTOs.Usuarios;
 using SGPla.Commons;
 using SGPla.Validations.Implementation;
+using SGPla.Models;
 
 public class UsuarioValidatorTests
 {
@@ -208,5 +209,270 @@ public class UsuarioValidatorTests
 
         Assert.NotNull(ex);
         Assert.Equal("No existe esa Área Académica.", ex.Message);
+    }
+
+    //CP-17
+    [Fact]
+    public async Task ObtenerUsuarioConValoresNulos()
+    {
+        var dto = new ReferenciaUsuarioDTO
+        {
+            IdUsuario = 0,
+            Rol = null
+        };
+
+        var ex = await Record.ExceptionAsync(() => _usuarioValidator.ValidarReferenciaAsync(dto));
+
+        Assert.NotNull(ex);
+        Assert.Equal("El IdUsuario es obligatorio.", ex.Message);
+    }
+
+    //CP-18
+    [Fact]
+    public async Task ObtenerUsuarioConRolInvalido()
+    {
+        var dto = new ReferenciaUsuarioDTO
+        {
+            IdUsuario = 101,
+            Rol = "Jefe de Carrera"
+        };
+
+        var ex = await Record.ExceptionAsync(() => _usuarioValidator.ValidarReferenciaAsync(dto));
+
+        Assert.NotNull(ex);
+        Assert.Equal("El rol no es válido.", ex.Message);
+    }
+
+    //CP-19
+    [Fact]
+    public async Task ObtenerUsuarioInexistente()
+    {
+        var dto = new ReferenciaUsuarioDTO
+        {
+            IdUsuario = 6767,
+            Rol = Constantes.CoordinadorDgaa
+        };
+
+        _coordinadorDgaaRepositoryMock
+            .Setup(repository => repository.ObtenerPorIdAsync(dto.IdUsuario))
+            .ReturnsAsync((CoordinadorDgaa?)null);
+
+        var ex = await Record.ExceptionAsync(() => _usuarioValidator.ValidarReferenciaAsync(dto));
+
+        Assert.NotNull(ex);
+        Assert.Equal("El Usuario no existe.", ex.Message);
+    }
+
+    //CP-22
+    [Fact]
+    public async Task EditarCoordinadorEaConValoresNulos()
+    {
+        var dto = new EditarUsuarioDTO
+        {
+            IdUsuario = 0,
+            Rol = null,
+            Nombre = null,
+            Cargo = null,
+            IdEntidadAcademica = 0
+        };
+
+        var ex = await Record.ExceptionAsync(() => _usuarioValidator.ValidarEdicionAsync(dto));
+
+        Assert.NotNull(ex);
+        Assert.Equal("La IdUsuario es obligatoria.", ex.Message);
+    }
+
+    //CP-23
+    [Fact]
+    public async Task EditarUsuarioConRolInvalido()
+    {
+        var dto = new EditarUsuarioDTO
+        {
+            IdUsuario = 534,
+            Rol = "Compositor",
+            Nombre = "Miguel Ángel Bocanegra",
+            Cargo = "Coordinador",
+            IdEntidadAcademica = 200
+        };
+
+        var ex = await Record.ExceptionAsync(() => _usuarioValidator.ValidarEdicionAsync(dto));
+
+        Assert.NotNull(ex);
+        Assert.Equal("El rol no es válido.", ex.Message);
+    }
+
+    //CP-24
+    [Fact]
+    public async Task EditarCoordinadorDgaaConIdEntidadAcademica()
+    {
+        var dto = new EditarUsuarioDTO
+        {
+            IdUsuario = 453,
+            Rol = Constantes.CoordinadorDgaa,
+            Nombre = "Luisa Londóñes",
+            Cargo = "Administradora Jefe",
+            IdEntidadAcademica = 2
+        };
+
+        _coordinadorDgaaRepositoryMock
+            .Setup(repository => repository.ObtenerPorIdAsync(dto.IdUsuario))
+            .ReturnsAsync(new CoordinadorDgaa { IdCoordinadorDgaa = 453, IdAreaAcademica = 2, Nombre = "Luisa Londoño", Correo = "luisa@uv.mx", Cargo = "Administradora Jefe" });
+
+        var ex = await Record.ExceptionAsync(() => _usuarioValidator.ValidarEdicionAsync(dto));
+
+        Assert.NotNull(ex);
+        Assert.Equal("El rol no corresponde con la dependencia editada.", ex.Message);
+    }
+
+    //CP-25
+    [Fact]
+    public async Task EditarCoordinadorEaConIdAreaAcademica()
+    {
+        var dto = new EditarUsuarioDTO
+        {
+            IdUsuario = 534,
+            Rol = Constantes.CoordinadorEa,
+            Nombre = "Miguel Ángel Bocanegra",
+            Cargo = "Coordinador",
+            IdAreaAcademica = 200
+        };
+
+        _coordinadorEaRepositoryMock
+            .Setup(repository => repository.ObtenerPorIdAsync(dto.IdUsuario))
+            .ReturnsAsync(new CoordinadorEa { IdCoordinadorEa = 534, IdEntidadAcademica = 200, Nombre = "Ángel Bocanegra", Correo = "angel@uv.mx", Cargo = "Jefe de Unidad" });
+
+        var ex = await Record.ExceptionAsync(() => _usuarioValidator.ValidarEdicionAsync(dto));
+
+        Assert.NotNull(ex);
+        Assert.Equal("El rol no corresponde con la dependencia editada.", ex.Message);
+    }
+
+    //CP-26
+    [Fact]
+    public async Task EditarCoordinadorEaConEntidadAcademicaInexistente()
+    {
+        var dto = new EditarUsuarioDTO
+        {
+            IdUsuario = 534,
+            Rol = Constantes.CoordinadorEa,
+            Nombre = "Miguel Ángel Bocanegra",
+            Cargo = "Coordinador",
+            IdEntidadAcademica = 76
+        };
+
+        _coordinadorEaRepositoryMock
+            .Setup(repository => repository.ObtenerPorIdAsync(dto.IdUsuario))
+            .ReturnsAsync(new CoordinadorEa { IdCoordinadorEa = 534, IdEntidadAcademica = 200, Nombre = "Ángel Bocanegra", Correo = "angel@uv.mx", Cargo = "Jefe de Unidad" });
+
+        _entidadAcademicaRepositoryMock
+            .Setup(repository => repository.ExistePorIdAsync(76))
+            .ReturnsAsync(false);
+
+        var ex = await Record.ExceptionAsync(() => _usuarioValidator.ValidarEdicionAsync(dto));
+
+        Assert.NotNull(ex);
+        Assert.Equal("No existe esa Entidad Académica.", ex.Message);
+    }
+
+    //CP-27
+    [Fact]
+    public async Task EditarCoordinadorDgaaConAreaAcademicaInexistente()
+    {
+        var dto = new EditarUsuarioDTO
+        {
+            IdUsuario = 453,
+            Rol = Constantes.CoordinadorDgaa,
+            Nombre = "Luisa Londóñes",
+            Cargo = "Administradora Jefe",
+            IdAreaAcademica = 10
+        };
+
+        _coordinadorDgaaRepositoryMock
+            .Setup(repository => repository.ObtenerPorIdAsync(dto.IdUsuario))
+            .ReturnsAsync(new CoordinadorDgaa { IdCoordinadorDgaa = 453, IdAreaAcademica = 2, Nombre = "Luisa Londoño", Correo = "luisa@uv.mx", Cargo = "Administradora Jefe" });
+
+        _areaAcademicaRepositoryMock
+            .Setup(repository => repository.ExistePorIdAsync(10))
+            .ReturnsAsync(false);
+
+        var ex = await Record.ExceptionAsync(() => _usuarioValidator.ValidarEdicionAsync(dto));
+
+        Assert.NotNull(ex);
+        Assert.Equal("No existe esa Área Académica.", ex.Message);
+    }
+
+    //CP-28
+    [Fact]
+    public async Task EditarUsuarioInexistente()
+    {
+        var dto = new EditarUsuarioDTO
+        {
+            IdUsuario = 912,
+            Rol = Constantes.CoordinadorDgaa,
+            Nombre = "Luisa Londóñes",
+            Cargo = "Administradora Jefe",
+            IdAreaAcademica = 10
+        };
+
+        _coordinadorDgaaRepositoryMock
+            .Setup(repository => repository.ObtenerPorIdAsync(dto.IdUsuario))
+            .ReturnsAsync((CoordinadorDgaa?)null);
+
+        var ex = await Record.ExceptionAsync(() => _usuarioValidator.ValidarEdicionAsync(dto));
+
+        Assert.NotNull(ex);
+        Assert.Equal("No existe ese usuario.", ex.Message);
+    }
+
+    //CP-31
+    [Fact]
+    public async Task EliminarUsuarioConValoresNulos()
+    {
+        var dto = new ReferenciaUsuarioDTO
+        {
+            IdUsuario = 0,
+            Rol = null
+        };
+
+        var ex = await Record.ExceptionAsync(() => _usuarioValidator.ValidarReferenciaAsync(dto));
+
+        Assert.NotNull(ex);
+        Assert.Equal("El IdUsuario es obligatorio.", ex.Message);
+    }
+
+    //CP-32
+    [Fact]
+    public async Task EliminarUsuarioConRolInvalido()
+    {
+        var dto = new ReferenciaUsuarioDTO
+        {
+            IdUsuario = 101,
+            Rol = "Jefe de Carrera"
+        };
+
+        var ex = await Record.ExceptionAsync(() => _usuarioValidator.ValidarReferenciaAsync(dto));
+
+        Assert.NotNull(ex);
+        Assert.Equal("El rol no es válido.", ex.Message);
+    }
+
+    //CP-33
+    [Fact]
+    public async Task EliminarUsuarioInexistente()
+    {
+        var dto = new ReferenciaUsuarioDTO
+        {
+            IdUsuario = 6767,
+            Rol = Constantes.CoordinadorDgaa
+        };
+
+        _coordinadorDgaaRepositoryMock
+            .Setup(repository => repository.ObtenerPorIdAsync(dto.IdUsuario))
+            .ReturnsAsync((CoordinadorDgaa?)null);
+
+        var ex = await Record.ExceptionAsync(() => _usuarioValidator.ValidarReferenciaAsync(dto));
+
+        Assert.NotNull(ex);
+        Assert.Equal("El Usuario no existe.", ex.Message);
     }
 }
