4d86ba4ee4e6b88f1cbef2c2f24faaab6d99da94
SaulBT <barragansaul26n@gmail.com>
2026-04-13 12:38:16 -0600

    feat: DTO para crear Area

---
+++ SGPla/Models/DTOs/AreaAcademica/CrearAreaAcademicaDTO.cs
@@ -0,0 +1,15 @@
+﻿namespace SGPla.Models.DTOs.AreaAcademica
+{
+    public class CrearAreaAcademicaDTO
+    {
+        public string Nombre { get; set; } = string.Empty;
+        public string CalleNumero { get; set; } = string.Empty;
+        public string Colonia { get; set; } = string.Empty;
+        public string Cp { get; set; } = string.Empty;
+        public string Municipio { get; set; } = string.Empty;
+        public string Telefono { get; set; } = string.Empty;
+        public string Conmutador { get; set; } = string.Empty;
+        public string Extension { get; set; } = string.Empty;
+        public string Fax { get; set; } = string.Empty;
+    }
+}
