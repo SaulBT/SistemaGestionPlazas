dd95cdd4017308c8a420a830ed76a45f7d2e60d4
SaulBT <barragansaul26n@gmail.com>
2026-04-13 12:33:13 -0600

    feat: DTOs de Área Académica
    DatosAreaAcademica es para crear, ver y editar.
    ListaAreaAcademica es para lo que se muestra en la lista.

---
+++ SGPla/Models/DTOs/AreaAcademica/ListaAreaAcademica.cs
@@ -0,0 +1,9 @@
+﻿namespace SGPla.Models.DTOs.AreaAcademica
+{
+    public class ListaAreaAcademica
+    {
+        public string Nombre { get; set; } = string.Empty;
+        public string Domicilio { get; set; } = string.Empty;
+        public string Telefono { get; set; } = string.Empty;
+    }
+}
