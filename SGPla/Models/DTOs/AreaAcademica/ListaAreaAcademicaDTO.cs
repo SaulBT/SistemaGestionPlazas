a076902685699dc8d799ef8574627a0d2b824b9d
SaulBT <barragansaul26n@gmail.com>
2026-04-13 12:59:14 -0600

    up: Id en ListaAreaAcademica

--- SGPla/Models/DTOs/AreaAcademica/ListaAreaAcademicaDTO.cs
+++ SGPla/Models/DTOs/AreaAcademica/ListaAreaAcademicaDTO.cs
@@ -2,6 +2,7 @@
 {
     public class ListaAreaAcademicaDTO
     {
+        public int IdAreaAcademica { get; set; } = 0;
         public string Nombre { get; set; } = string.Empty;
         public string Domicilio { get; set; } = string.Empty;
         public string Telefono { get; set; } = string.Empty;
