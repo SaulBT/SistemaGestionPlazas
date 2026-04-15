d4063a695e2b2b14f63c39a865df3ee4e92ce305
SaulBT <barragansaul26n@gmail.com>
2026-04-15 10:47:43 -0600

    up: método en el repositorio para obtener por nombre

--- SGPla/Repositories/Interfaces/IAreaAcademicaRepository.cs
+++ SGPla/Repositories/Interfaces/IAreaAcademicaRepository.cs
@@ -5,6 +5,7 @@ namespace SGPla.Repositories.Interfaces
     public interface IAreaAcademicaRepository
     {
         Task<List<AreaAcademica>> ObtenerTodosAsync();
+        Task<List<AreaAcademica>> ObtenerPorNombreAsync(string nombre);
         Task<AreaAcademica?> ObtenerPorIdAsync(int idAreaAcademica);
         Task<bool> ExistePorIdAsync(int idAreaAcademica);
         Task<AreaAcademica> CrearAsync(AreaAcademica areaAcademica);
---
+++ SGPla/Services/Interfaces/IAreaAcademicaService.cs
@@ -0,0 +1,14 @@
+﻿using SGPla.Models.DTOs.AreaAcademica;
+
+namespace SGPla.Services.Interfaces
+{
+    public interface IAreaAcademicaService
+    {
+        Task<int> CrearAsync(CrearAreaAcademicaDTO dto);
+        Task<List<ListaAreaAcademicaDTO>> ObtenerTodasAsync();
+        Task<List<ListaAreaAcademicaDTO>> ObtenerPorNombreAsync(string nombre);
+        Task<DatosAreaAcademicaDTO> ObtenerPorIdAsync(int id);
+        Task EditarAsync(DatosAreaAcademicaDTO dto);
+        Task EliminarAsync(int id);
+    }
+}
