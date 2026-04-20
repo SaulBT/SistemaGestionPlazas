3c8250a64f803c35178f4c6f3167cceca98a9dbc
SaulBT <barragansaul26n@gmail.com>
2026-04-20 11:44:34 -0600

    up: agregadas constantes de Area y Entidad

--- SGPla/Commons/Constantes.cs
+++ SGPla/Commons/Constantes.cs
@@ -4,5 +4,8 @@
     {
         public const string CoordinadorEa = "Coordinador de Entidad Académica";
         public const string CoordinadorDgaa = "Coordinador de Área Académica";
+
+        public const string IdEntidadAcademica = "IdEntidadAcademica";
+        public const string IdAreaAcademica = "IdAreaAcademica";
     }
 }
