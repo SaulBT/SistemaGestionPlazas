7b460957c02d75cc5a0fa6a196eb70ce86c69225
LPablo-NRG <luispablolagunesnoriega@gmail.com>
2026-04-22 11:53:35 -0600

    feat: se agregó inputField

---
+++ SGPla/Models/InputFieldModel.cs
@@ -0,0 +1,10 @@
+﻿public class InputFieldModel
+{
+    public string Id { get; set; }
+    public string Name { get; set; }
+    public string Tipo { get; set; } = "text";
+    public string Valor { get; set; }
+    public string Placeholder { get; set; }
+    public string Label { get; set; }
+    public bool Disabled { get; set; }
+}
---
+++ SGPla/ViewComponents/InputFieldViewComponent.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SGPla.Models;
+
+namespace SGPla.ViewComponents
+{
+    public class InputFieldViewComponent : ViewComponent
+    {
+
+        public IViewComponentResult Invoke(
+            string name,
+            string label,
+            string placeholder = "",
+            string tipo = "text",
+            string valor = "",
+            bool disabled = false,
+            string id = null)
+        {
+            var model = new InputFieldModel
+            {
+                Name = name,
+                Label = label,
+                Placeholder = placeholder,
+                Tipo = tipo,
+                Valor = valor,
+                Disabled = disabled,
+                Id = id ?? name
+            };
+
+            return View(model);
+        }
+    }
+}
