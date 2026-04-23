867eb93dc81b89d3d21a67a3dfbaba8f9fa14ef1
LPablo-NRG <luispablolagunesnoriega@gmail.com>
2026-04-23 13:27:03 -0600

    feat: Se terminaron de implementar componentes de campos

---
+++ SGPla/Models/SwitchFieldModel.cs
@@ -0,0 +1,12 @@
+﻿namespace SGPla.Models
+{
+    public class SwitchFieldModel
+    {
+        public string Id { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string Label { get; set; } = string.Empty;
+
+        public bool Checked { get; set; }
+        public bool Disabled { get; set; }
+    }
+}
---
+++ SGPla/ViewComponents/TextAreaFieldViewComponent.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SGPla.Models;
+
+namespace SGPla.ViewComponents
+{
+    public class TextAreaFieldViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(TextAreaFieldModel model)
+        {
+            model.Id ??= model.Name;
+            return View(model);
+        }
+    }
+}
