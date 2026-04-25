fce7d43370fe48b7a8f6ecd1611f7999cbd8e701
LPablo-NRG <luispablolagunesnoriega@gmail.com>
2026-04-24 18:24:46 -0600

    feat: se agregó componente de tablas

---
+++ SGPla/Models/TableModel.cs
@@ -0,0 +1,24 @@
+﻿public class TableModel
+{
+    public List<string> Headers { get; set; } = new();
+    public List<TableRowModel> Rows { get; set; } = new();
+}
+
+public class TableRowModel
+{
+    public List<TableCellModel> Cells { get; set; } = new();
+}
+
+public class TableCellModel
+{
+    public string Value { get; set; } = string.Empty;
+
+    // Para acciones
+    public List<TableActionModel>? Actions { get; set; }
+}
+
+public class TableActionModel
+{
+    public string Accion { get; set; } = ""; // editar, eliminar, etc.
+    public string Url { get; set; } = "";
+}
---
+++ SGPla/ViewComponents/TableViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SGPla.ViewComponents
+{
+    public class TableViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(TableModel model)
+        {
+            return View(model);
+        }
+    }
+}
