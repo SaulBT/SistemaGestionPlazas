46ad993c3c64f63d0e6cfe7642a06d05a9f6235a
LPablo-NRG <luispablolagunesnoriega@gmail.com>
2026-04-24 19:42:25 -0600

    up: se crearon carpetas específicas para los modelos de componentes y de vistas

---
+++ SGPla/Models/ViewModels/UsuariosIndexViewModel.cs
@@ -0,0 +1,13 @@
+﻿using SGPla.Models.Components;
+
+namespace SGPla.Models.ViewModels
+{
+    public class UsuariosIndexViewModel
+    {
+        public TableModel Table { get; set; }
+        public List<OptionModel> Regiones { get; set; }
+        public List<OptionModel> Areas { get; set; }
+        public List<OptionModel> Entidades { get; set; }
+    }
+}
+
