b76f72462abeb53e822a22c6bef415dfa0f0915b
SaulBT <barragansaul26n@gmail.com>
2026-04-28 12:26:48 -0600

    feat: dtos de plan de estudio con id archivo.
    Tanto crear como editar tienen o pueden enviar un archivo nuevo.

--- SGPla/Models/DTOs/PlanEstudios/CrearPlanEstudiosDTO.cs
+++ SGPla/Models/DTOs/PlanEstudios/CrearPlanEstudiosDTO.cs
@@ -5,6 +5,7 @@
         public int IdProgramaEducativo { get; set; }
         public string Nombre { get; set; } = string.Empty;
         public string Sistema { get; set; } = string.Empty;
+        public ArchivoPlanEstudiosDTO Archivo { get; set; } = null!;
         public List<AgregarExperienciaEducativaDTO> ExperienciasEducativas { get; set; } = [];
     }
 }
--- SGPla/Models/DTOs/PlanEstudios/EditarPlanEstudiosDTO.cs
+++ SGPla/Models/DTOs/PlanEstudios/EditarPlanEstudiosDTO.cs
@@ -3,6 +3,8 @@
     public class EditarPlanEstudiosDTO
     {
         public int IdPlanEstudios { get; set; }
+        public bool NuevaLista { get; set; }
+        public ArchivoPlanEstudiosDTO? Archivo { get; set; }
         public List<AgregarExperienciaEducativaDTO> ExperienciasNuevas = [];
         public List<DatosExperienciaEducativaDTO> ExperienciasEditadas = [];
         public List<int> IdsExperienciasEliminadas = [];
