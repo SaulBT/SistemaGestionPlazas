c0e57030ed04685a1880a8882713ae0a6020d0c1
SaulBT <barragansaul26n@gmail.com>
2026-04-28 12:26:19 -0600

    feat: dto para obtener metadatos de archivo

---
+++ SGPla/Models/DTOs/Archivo/DatosArchivoGuardadoDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace SGPla.Models.DTOs.Archivo
+{
+    public class DatosArchivoGuardadoDTO
+    {
+        public string NombreOriginal { get; set; } = null!;
+        public string Ruta { get; set; } = null!;
+        public string Tipo { get; set; } = null!;
+        public long Tamanio { get; set; }
+    }
+}
