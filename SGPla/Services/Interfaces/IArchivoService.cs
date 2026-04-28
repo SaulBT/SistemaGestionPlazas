1d8e02d4075172c0d71c04e765e9b11bf401c47f
SaulBT <barragansaul26n@gmail.com>
2026-04-28 12:27:14 -0600

    feat: Servicio de archivos que guardan en disco

---
+++ SGPla/Services/Interfaces/IArchivoService.cs
@@ -0,0 +1,10 @@
+﻿using SGPla.Models.DTOs.Archivo;
+
+namespace SGPla.Services.Interfaces
+{
+    public interface IArchivoService
+    {
+        Task<DatosArchivoGuardadoDTO> GuardarAsync(Stream archivo, string nombreOriginal, string carpeta);
+        Task EliminarAsync(string rutaRelativa);
+    }
+}
