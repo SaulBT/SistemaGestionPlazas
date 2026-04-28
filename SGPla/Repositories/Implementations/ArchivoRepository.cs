0de574cf06c16c7b2614a220a8949bc3d076d2d8
SaulBT <barragansaul26n@gmail.com>
2026-04-28 12:27:32 -0600

    feat: Repositorio de archivos que guardan en bd

---
+++ SGPla/Repositories/Implementations/ArchivoRepository.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SGPla.Data;
+using SGPla.Models;
+using SGPla.Repositories.Interfaces;
+
+namespace SGPla.Repositories.Implementations
+{
+    public class ArchivoRepository : IArchivoRepository
+    {
+        private readonly GestionDePlazasDbContext _context;
+
+        public ArchivoRepository(GestionDePlazasDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Archivo> CrearAsync(Archivo archivo)
+        {
+            await _context.Archivo.AddAsync(archivo);
+            await _context.SaveChangesAsync();
+            return archivo;
+        }
+
+        public async Task<Archivo?> ObtenerPorIdAsync(int idArchivo)
+        {
+            return await _context.Archivo
+                .FirstOrDefaultAsync(a => a.IdArchivo == idArchivo);
+        }
+
+        public async Task ActualizarAsync(Archivo archivo)
+        {
+            _context.Archivo.Update(archivo);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task EliminarAsync(Archivo archivo)
+        {
+            ArgumentNullException.ThrowIfNull(archivo);
+
+            _context.Archivo.Remove(archivo);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
