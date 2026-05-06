41710557adf2d26f348477be2357eeac208fa0f4
SaulBT <barragansaul26n@gmail.com>
2026-05-06 13:55:54 -0600

    feat: modelo del paso 1 de Crear Plan

---
+++ SGPla/Models/ViewModels/PlanesEstudios/CargarPlanEstudiosPaso1ViewModel.cs
@@ -0,0 +1,20 @@
+﻿using SGPla.Models.Components;
+
+namespace SGPla.Models.ViewModels.PlanesEstudios
+{
+    public class CargarPlanEstudiosPaso1ViewModel
+    {
+        public string RegionSeleccionada { get; set; } = string.Empty;
+        public int? AreaSeleccionada { get; set; }
+        public int? EntidadSeleccionada { get; set; }
+        public int? ProgramaSeleccionado { get; set; }
+        public string Plan { get; set; } = string.Empty;
+        public string SistemaSeleccionado { get; set; } = string.Empty;
+
+        public List<OptionModel> ListaRegiones { get; set; } = [];
+        public List<OptionModel> ListaAreas { get; set; } = [];
+        public List<OptionModel> ListaEntidades { get; set; } = [];
+        public List<OptionModel> ListaProgramas { get; set; } = [];
+        public List<OptionModel> ListaSistema { get; set; } = [];
+    }
+}
