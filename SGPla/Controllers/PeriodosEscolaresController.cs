eeccc5411b81edcf2a99526a28a0d6958779e57c
francoroguez <118698321+roguez26@users.noreply.github.com>
2026-05-06 12:55:44 -0600

    up: se agregó interfaz gráfica

--- SGPla/Controllers/PeriodosEscolaresController.cs
+++ SGPla/Controllers/PeriodosEscolaresController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using SGPla.Models.DTOs.Articulo;
 using SGPla.Models.DTOs.PeriodoEscolar;
 using SGPla.Models.DTOs.ProgramaEducativo;
+using SGPla.Models.ViewModels.PeriodosEscolares;
 using SGPla.Services.Implementations;
 using SGPla.Services.Interfaces;
 
@@ -9,17 +12,78 @@ namespace SGPla.Controllers
     public class PeriodosEscolaresController : Controller
     {
         private readonly IPeriodoEscolarService _periodoEscolarService;
+        private readonly ILogger<PeriodosEscolaresController> _logger;
 
-        public PeriodosEscolaresController(IPeriodoEscolarService periodoEscolarService)
+        public PeriodosEscolaresController(IPeriodoEscolarService periodoEscolarService, ILogger<PeriodosEscolaresController> logger    )
         {
             _periodoEscolarService = periodoEscolarService;
+            _logger = logger;
         }
 
-        public async Task<IActionResult> Index(BuscarPeriodoEscolarDTO filtro)
+        public async Task<IActionResult> Index(int? anio, string? periodo)
         {
-            var programasEducativos = await _periodoEscolarService.BuscarPorFiltroAsync(filtro);
 
-            return View(programasEducativos);
+            return View(new IndexViewModel
+            {
+                Table = await LlenarTabla(anio, periodo),
+                Anio = anio,
+                Periodo = periodo
+            });
+        }
+
+        private async Task<TableModel> LlenarTabla(int? anio, string? periodo)
+        {
+            try
+            {
+                IEnumerable<DetallesPeriodoEscolarDTO> periodos;
+
+                var filtro = new BuscarPeriodoEscolarDTO
+                {
+                    Anio = anio,
+                    Periodo = periodo
+                };
+
+                periodos = await _periodoEscolarService.BuscarPorFiltroAsync(filtro);
+
+                return new TableModel
+                {
+                    Headers = new List<string> { "Código", "Año", "Periodo", "Acciones" },
+                    Rows = periodos.Select(a => new TableRowModel
+                    {
+                        Cells = new List<TableCellModel>
+                    {
+                        new TableCellModel { Value = a.Codigo},
+                        new TableCellModel { Value = a.Anio.ToString() },
+                        new TableCellModel { Value = a.Periodo },
+                        new TableCellModel
+                        {
+                            Actions = new List<TableActionModel>
+                            {
+                                new TableActionModel()
+                                {
+                                    Accion = "Editar",
+                                    OnClick = $"abrirModalEditarPeriodoEscolar({a.IdPeriodoEscolar}, '{a.Anio}', '{a.Periodo}')"
+                                },
+                                new TableActionModel()
+                                {
+                                    Accion = "Eliminar",
+                                    OnClick = $"abrirModalConfirmacion('¿Desea eliminar este periodo?', function() {{ eliminarPeriodoEscolar({a.IdPeriodoEscolar}); }})"
+                                }
+                            }
+                        }
+                    },
+
+                    }).ToList()
+                };
+            }
+
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error al llenar la tabla de artículos");
+                TempData["Error"] = "Ocurrió un error al cargar los artículos. Por favor, inténtelo de nuevo más tarde.";
+
+                return new TableModel();
+            }
         }
 
 
@@ -92,9 +156,9 @@ namespace SGPla.Controllers
             {
                 var resultado = await _periodoEscolarService.EliminarAsync(id);
                 if (resultado)
-                    TempData["Success"] = $"Periodo Escolar eliminado exitosamente con ID: {id}";
-                else
-                    TempData["Error"] = $"No se pudo eliminar el Periodo Escolar con ID: {id}";
+                {
+                    TempData["Success"] = $"Periodo eliminado exitosamente";
+                }
             }
             catch (ArgumentException ex)
             {
---
+++ SGPla/Models/ViewModels/PeriodosEscolares/IndexViewModel.cs
@@ -0,0 +1,20 @@
+﻿using SGPla.Models.Components;
+
+namespace SGPla.Models.ViewModels.PeriodosEscolares
+{
+    public class IndexViewModel
+    {
+        public TableModel Table { get; set; }
+
+        public List<OptionModel> Periodos { get; set; }
+
+        public int? Anio { get; set; }
+
+        public string? Periodo { get; set; }
+
+
+        public PeriodoEscolarFormularioViewModel Formulario { get; set; }
+
+
+    }
+}
