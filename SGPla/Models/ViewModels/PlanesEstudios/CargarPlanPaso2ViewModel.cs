4b556de1c72101d0653bd54b977cdacd48573d01
SaulBT <barragansaul26n@gmail.com>
2026-05-11 09:57:08 -0600

    feat: vista de Cargar Plan Paso 2

---
+++ SGPla/Models/ViewModels/PlanesEstudios/CargarPlanPaso2ViewModel.cs
@@ -0,0 +1,15 @@
+﻿using SGPla.Models.DTOs.PlanEstudios;
+
+namespace SGPla.Models.ViewModels.PlanesEstudios
+{
+    public class CargarPlanPaso2ViewModel
+    {
+        public string Region { get; set; } = string.Empty;
+        public string Area { get; set; } = string.Empty;
+        public string ProgramaEducativo { get; set; } = string.Empty;
+        public string Plan { get; set; } = string.Empty;
+        public string Sistema { get; set; } = string.Empty;
+        public List<DatosExperienciaEducativaDTO> Experiencias { get; set; } = [];
+        public IFormFile Archivo { get; set; }
+    }
+}
