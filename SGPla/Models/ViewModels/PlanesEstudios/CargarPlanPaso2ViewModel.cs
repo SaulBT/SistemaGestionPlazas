708455b06bfd847240c3064cb8e383e55c53d615
SaulBT <barragansaul26n@gmail.com>
2026-05-13 14:04:24 -0600

    feat: modales en vista del paso 2

--- SGPla/Models/ViewModels/PlanesEstudios/CargarPlanPaso2ViewModel.cs
+++ SGPla/Models/ViewModels/PlanesEstudios/CargarPlanPaso2ViewModel.cs
@@ -12,6 +12,7 @@ namespace SGPla.Models.ViewModels.PlanesEstudios
         public int IdProgramaEducativo { get; set; }
         public string Plan { get; set; } = string.Empty;
         public string Sistema { get; set; } = string.Empty;
+        public FormularioExperienciaEducativaViewModel Formulario { get; set; }
         public IFormFile Archivo { get; set; }
         public TableModel Table { get; set; }
     }
