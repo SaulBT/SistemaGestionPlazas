f509cdd647b1f498a70c2079a333e50fb81a72b3
SaulBT <barragansaul26n@gmail.com>
2026-05-13 14:03:59 -0600

    feat: controller js para gestionar ee

---
+++ SGPla/wwwroot/js/gestionarExperiencias.js
@@ -0,0 +1,82 @@
+﻿const codigoAgregar = document.getElementById("CodigoAgregar");
+const nombreAgregar = document.getElementById("NombreAgregar");
+const perfilDocenteAgregar = document.getElementById("PerfilDocenteAgregar");
+
+const codigoEditar = document.getElementById("CodigoEditar");
+const nombreEditar = document.getElementById("NombreEditar");
+const perfilDocenteEditar = document.getElementById("PerfilDocenteEditar");
+
+const tbody = document.querySelector("#tabla tbody");
+var codigoOriginal = "";
+
+function abrirModalAgregarExperiencia() {
+    abrirModal("modalAgregarExperiencia");
+}
+
+async function agregarExperienciaCreacion() {
+    const response = await fetch(`${UrlAgregarExperiencia}?codigo=${encodeURIComponent(codigoAgregar.value)}&nombre=${encodeURIComponent(nombreAgregar.value)}&perfilDocente=${encodeURIComponent(perfilDocenteAgregar.value)}`);
+    const experiencia = await response.json();
+
+    const fila = generarFila(experiencia.codigo, experiencia.nombre, experiencia.perfilDocente);
+    tbody.appendChild(fila);
+
+    cerrarModal("modalAgregarExperiencia")
+}
+
+function abrirModalEditarExperiencia(codigo, nombre, perfilDocente) {
+    codigoOriginal = codigo;
+    codigoEditar.value = codigo;
+    nombreEditar.value = nombre;
+    perfilDocenteEditar.value = perfilDocente;
+
+    abrirModal("modalEditarExperiencia");
+}
+
+async function editarExperienciaCreacion() {
+    const response = await fetch(`${UrlEditarExperiencia}?codigo=${encodeURIComponent(codigoEditar.value)}&nombre=${encodeURIComponent(nombreEditar.value)}&perfilDocente=${encodeURIComponent(perfilDocenteEditar.value)}&codigoOriginal=${encodeURIComponent(codigoOriginal)}`);
+    const experiencias = await response.json();
+
+    tbody.innerHTML = "";
+    experiencias.forEach(ee => {
+        const fila = generarFila(ee.codigo, ee.nombre, ee.perfilDocente);
+        tbody.appendChild(fila);
+
+        cerrarModal("modalEditarExperiencia")
+    });
+}
+
+function generarFila(codigo, nombre, perfilDocente) {
+    const fila = document.createElement("tr");
+    fila.innerHTML = `
+            <td>${codigo}</td>
+            <td>${nombre}</td>
+            <td>
+                <div class="table-actions">
+                    <button
+                        type="button"
+                        class="boton-primario boton-icono"
+                        onclick="abrirModalPerfilDocente('${perfilDocente}')">
+                        <i class="bi bi-info-circle-fill"></i>
+                    </button>
+                </div>
+            </td>
+            <td>
+                <div class="table-actions">
+                    <button
+                        type="button"
+                        class="boton-primario boton-icono"
+                        onclick="abrirModalEditarExperiencia('${codigo}', '${nombre}', '${perfilDocente}')"">
+
+                        <i class="bi bi-pencil-fill"></i>
+                    </button>
+                    <button
+                        type="button"
+                        class="boton-primario boton-icono">
+                        <i class="bi bi-trash-fill"></i>
+                    </button>
+                </div>
+            </td>
+        `;
+
+    return fila;
+}
