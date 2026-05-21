a746173f03df226f04c075da1dda731c0693e218
SaulBT <barragansaul26n@gmail.com>
2026-05-21 12:55:34 -0600

    Update ArchivoDescargadoDTO.cs

--- SGPla/Models/DTOs/Archivo/ArchivoDescargadoDTO.cs
+++ SGPla/Models/DTOs/Archivo/ArchivoDescargadoDTO.cs
@@ -2,7 +2,7 @@
 {
     public class ArchivoDescargadoDTO
     {
-        public Stream Contenido { get; set; } = null!;
+        public string Ruta { get; set; } = null!;
         public string Nombre { get; set; } = null!;
         public string Tipo { get; set; } = null!;
     }
