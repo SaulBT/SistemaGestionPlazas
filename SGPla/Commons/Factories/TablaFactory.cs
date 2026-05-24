5802cb57a7f9506adc8f3ea98924914ef905e3d9
SaulBT <184581199+SaulBT@users.noreply.github.com>
2026-05-23 20:02:27 -0600

    feat: tabla factory

---
+++ SGPla/Commons/Factories/TablaFactory.cs
@@ -0,0 +1,30 @@
+﻿namespace SGPla.Commons.Factories
+{
+    public static class TablaFactory
+    {
+        public static TableModel GenerarTablaConMensaje(List<string> headers, string mensaje)
+        {
+            List<TableCellModel> cells = new();
+            cells.Add(new TableCellModel
+            {
+                Value = mensaje
+            });
+            for (int i = 0; i < headers.Count - 1; i++)
+            {
+                cells.Add(new TableCellModel());
+            }
+
+            return new TableModel
+            {
+                Headers = headers,
+                Rows = new TableRowModel[]
+                    {
+                        new TableRowModel
+                        {
+                            Cells = cells
+                        }
+                    }.ToList(),
+            };
+        }
+    }
+}
