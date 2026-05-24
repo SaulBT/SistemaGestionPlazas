0bff6c2e4e74ddad8bf6d759ed2be8239e2e5c9b
SaulBT <184581199+SaulBT@users.noreply.github.com>
2026-05-23 19:28:34 -0600

    feat: extension para lanzar errores
    Permite lanzar un log de error junto con el temp data con el mensaje del toast

---
+++ SGPla/Controllers/ControllerExtension.cs
@@ -0,0 +1,26 @@
+﻿namespace SGPla.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using SGPla.Commons;
+
+    public static class ControllerExtension
+    {
+        public static void LanzarError<T>(this Controller controller, ILogger<T> logger, Exception? ex, string ubicacion, string metodo, string log, string toast)
+        {
+            if (ex != null)
+                logger.LogError(ex, Constantes.LOGS_ESTRUCTURA, ubicacion, metodo, log);
+            else
+                logger.LogError(Constantes.LOGS_ESTRUCTURA, ubicacion, metodo, log);
+            controller.TempData["Error"] = toast;
+        }
+
+        public static void LanzarError<T>(this Controller controller, ILogger<T> logger, Exception? ex, string ubicacion, string metodo, string log)
+        {
+            if (ex != null)
+                logger.LogError(ex, Constantes.LOGS_ESTRUCTURA, ubicacion, metodo, log);
+            else
+                logger.LogError(Constantes.LOGS_ESTRUCTURA, ubicacion, metodo, log);
+            controller.TempData["Error"] = Constantes.TOAST_ERROR_GENERAL;
+        }
+    }
+}
