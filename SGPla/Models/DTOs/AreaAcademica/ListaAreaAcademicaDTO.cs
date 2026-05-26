440bcbe8454c6b8f634458ec18a38e6c2eba4207
SaulBT <barragansaul26n@gmail.com>
2026-05-26 10:30:36 -0600

    up: quitar datos de domicilio, conmutador y fax en areas

--- SGPla/Models/DTOs/AreaAcademica/ListaAreaAcademicaDTO.cs
+++ SGPla/Models/DTOs/AreaAcademica/ListaAreaAcademicaDTO.cs
@@ -4,7 +4,6 @@
     {
         public int IdAreaAcademica { get; set; } = 0;
         public string Nombre { get; set; } = string.Empty;
-        public string Domicilio { get; set; } = string.Empty;
         public string Telefono { get; set; } = string.Empty;
     }
 }
