aff75d6ec03ddf873f32682f78664f665257daa9
SaulBT <184581199+SaulBT@users.noreply.github.com>
2026-05-31 16:33:07 -0600

    up:  método de tabla experiencias.
    Se añadió un método que genera la tabla de experiencias con mensaje

--- SGPla/Commons/Factories/TablaFactory.cs
+++ SGPla/Commons/Factories/TablaFactory.cs
@@ -26,5 +26,30 @@
                     }.ToList(),
             };
         }
+
+        public static TableExperienciasModel GenerarTablaExperienciasConMensaje(List<string> headers, string mensaje)
+        {
+            List<TableCellModel> cells = new();
+            cells.Add(new TableCellModel
+            {
+                Value = mensaje
+            });
+            for (int i = 0; i < headers.Count - 1; i++)
+            {
+                cells.Add(new TableCellModel());
+            }
+
+            return new TableExperienciasModel
+            {
+                Headers = headers,
+                Rows = new TableExperienciasRowModel[]
+                    {
+                        new TableExperienciasRowModel
+                        {
+                            Cells = cells
+                        }
+                    }.ToList(),
+            };
+        }
     }
 }
