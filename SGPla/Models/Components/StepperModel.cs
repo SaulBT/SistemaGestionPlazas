b625536afe3923d30d702c65bede962cb41a5d6c
SaulBT <barragansaul26n@gmail.com>
2026-06-02 13:40:24 -0600

    feat: componente stepper añadido a las vistas de CargarPaso1 y CargarPaso2

---
+++ SGPla/Models/Components/StepperModel.cs
@@ -0,0 +1,9 @@
+﻿namespace SGPla.Models.Components
+{
+    public class StepperModel
+    {
+        public int PasoActual { get; set; }
+        public string Paso1Texto { get; set; } = string.Empty;
+        public string Paso2Texto { get; set; } = string.Empty;
+    }
+}
---
+++ SGPla/ViewComponents/StepperViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SGPla.Models.Components;
+
+namespace SGPla.ViewComponents
+{
+    public class StepperViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(StepperModel model)
+        {
+            return View(model);
+        }
+    }
+}
