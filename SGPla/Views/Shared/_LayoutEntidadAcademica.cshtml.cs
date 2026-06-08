8d0ee04605bbd97bba32b4d67e314cee727bdb11
LPablo-NRG <luispablolagunesnoriega@gmail.com>
2026-06-08 14:44:07 -0600

    feat: se implementó el layout de entidad academica desde la página /home

---
+++ SGPla/Views/Shared/_LayoutEntidadAcademica.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SGPla.Views.Shared
+{
+    public class _LayoutEntidadAcademicaModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
