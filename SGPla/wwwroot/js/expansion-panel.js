58c848ef933c5d6b8aa4cd0869f018c47c1e0a0b
LPablo-NRG <luispablolagunesnoriega@gmail.com>
2026-06-08 12:33:02 -0600

    feat: se creó el componente expansion panel

---
+++ SGPla/wwwroot/js/expansion-panel.js
@@ -0,0 +1,49 @@
+﻿function toggleExpansionPanel(panelId) {
+
+    const panel = document.getElementById(panelId);
+    const icon = document.getElementById(`icon-${panelId}`);
+
+    const isOpen = panel.classList.toggle("show");
+
+    actualizarIcono(icon, isOpen);
+
+    localStorage.setItem(
+        `expansion-panel-${panelId}`,
+        isOpen
+    );
+}
+
+function actualizarIcono(icon, isOpen) {
+
+    if (isOpen) {
+        icon.textContent = "▼";
+    }
+    else {
+        icon.textContent = "►";
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    const panels = document.querySelectorAll(".expansion-content");
+
+    panels.forEach(panel => {
+
+        const panelId = panel.id;
+
+        const savedState = localStorage.getItem(
+            `expansion-panel-${panelId}`
+        );
+
+        if (savedState === null)
+            return;
+
+        const icon = document.getElementById(`icon-${panelId}`);
+
+        const isOpen = savedState === "true";
+
+        panel.classList.toggle("show", isOpen);
+
+        actualizarIcono(icon, isOpen);
+    });
+});
