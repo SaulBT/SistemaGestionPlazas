b71da3ed970bba4c5a6aa10e4360cc7bea408b0b
LPablo-NRG <luispablolagunesnoriega@gmail.com>
2026-06-08 13:37:09 -0600

    feat: se creó el componente "tabs"

---
+++ SGPla/Views/Home/_Profesores.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SGPla.Views.Home
+{
+    public class _ProfesoresModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
