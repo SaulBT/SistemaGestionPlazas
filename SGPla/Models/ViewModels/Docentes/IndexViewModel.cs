1ac9513bf09f04a6db62d475eff794ec74cb0951
SaulBT <barragansaul26n@gmail.com>
2026-06-16 19:57:35 -0600

    feat: Vistas del CU-25
    Index, RegistrarDocente y Tabs de Index

---
+++ SGPla/Models/ViewModels/Docentes/IndexViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace SGPla.Models.ViewModels.Docentes
+{
+    public class IndexViewModel
+    {
+        public TabIndexViewModel TabDocentes { get; set; } = new TabIndexViewModel();
+        public TabIndexViewModel TabAspirantes { get; set; } = new TabIndexViewModel();
+        public int TabActivada { get; set; }
+    }
+}
---
+++ SGPla/Models/ViewModels/Docentes/RegistrarDocenteViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace SGPla.Models.ViewModels.Docentes
+{
+    public class RegistrarDocenteViewModel
+    {
+        public string Nombre { get; set; } = string.Empty;
+        public string DescripcionPerfil { get; set; } = string.Empty;
+        public IFormFile Archivo { get; set; }
+        public TableModel Tabla { get; set; } = new TableModel();
+        public string NumeroPersonal { get; set; } = string.Empty;
+        public string Puesto { get; set; } = string.Empty;
+        public bool Recarga { get; set; } = false;
+    }
+}
