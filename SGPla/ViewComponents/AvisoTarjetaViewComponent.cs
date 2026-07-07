f543853cb876b6764140f2fb2339a85268f47863
SaulBT <barragansaul26n@gmail.com>
2026-07-07 14:40:17 -0600

    feat: empezando la vista del index

---
+++ SGPla/ViewComponents/AvisoTarjetaViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SGPla.Models.Components;
+
+namespace SGPla.ViewComponents
+{
+    public class AvisoTarjetaViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(AvisoTarjetaModel model)
+        {
+            return View(model);
+        }
+    }
+}
