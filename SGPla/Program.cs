6bca09225040a7a97905b1dd396e6b0adee9f786
SaulBT <barragansaul26n@gmail.com>
2026-08-04 20:36:14 -0600

    pos ahí va

--- SGPla/Program.cs
+++ SGPla/Program.cs
@@ -122,7 +122,7 @@ app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Docentes}/{action=Index}/{id?}")
+    pattern: "{controller=Avisos}/{action=Index}/{id?}")
     .WithStaticAssets();
 
 Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
